6cc13c2e82b73154e5e295750bc6890391b91d3b
DaHua89 <85625355+DaHua89@users.noreply.github.com>
2022-10-07 15:40:36 +0200

    Update GetData_KunzeSuppa16.R

--- GetData_KunzeSuppa16.R
+++ GetData_KunzeSuppa16.R
@@ -258,11 +258,10 @@ labeling <- function(df){
 # defines negatives values as missing values. 
 # The function returns a modified dataframe. 
 recoding <- function(df){
-  df_main <- df %>% filter(syear %in% c(1992, 1994, 1996,   
-                                        1997, 1999, 2001, 2005, 
-                                        2007, 2009, 2011)) %>%
+  # Rausgenommen: 07.10.2022
+  % df_main <- df %>% filter(syear %in% c(1992, 1994, 1996, 1997, 1999, 2001, 2005, 2007, 2009, 2011)) %>%
     # Modifications on our 6 main variables 
-    mutate_at(c("culture", "cinema", "sports", "social", "help", "volunteer"),
+   df_main <- df %>%  mutate_at(c("culture", "cinema", "sports", "social", "help", "volunteer"),
               funs(recode(., '2'=4, '3'=3, '4'=2, '5'=1))) %>% # recode all variables' (.) values, whereas 2 becomes 4, 4 becomes 2 and 5 becomes 1 
     mutate_at(c("culture", "cinema", "sports", "social", "help", "volunteer"),
               funs(ifelse(.<0, NA, .)))  # recodes all variables (.) to NA, if they take values below zero (.<0)

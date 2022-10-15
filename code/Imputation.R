99842f349c27caf020e7c0e76e03bbc03bd98dd9
IsabellFtz <85625355+IsabellFtz@users.noreply.github.com>
2022-10-15 20:34:22 +0200

    final changes

--- code/Imputation.R
+++ code/Imputation.R
@@ -4,13 +4,13 @@
 ********************* Imputation.R (master: MASTER.R) **************************
 ********************************************************************************
 Authors:    T. Bethge, I. Fetzer, M. Paul  
-Data:       SOEP (v30, teaching version [DOI: 10.5684/soep.v30])
+Data:       SOEP (v37, teaching version [DOI: 10.5684/soep.core.v37t])
 Note:       Please refer to the MASTER.R file to run the present R script. 
 '
 
 # 1 Load in data ---------------------------------------------------------------
 df=list()
-toload <- c("our_dataset")
+toload <- c("main_dataset")
 for( i in 1:length(toload)){
   i <- toload[i]
   if (exists(i)) {
@@ -39,46 +39,48 @@ outcomes <- c('culture', 'cinema', 'sports', 'help', 'volunteer', 'social')
 years <- c(1992, 1994, 1996, 1997, 2001, 2005, 2007, 2009, 2011)
 no_factor <- c("cid", "hid", "pid", "yearsedu", "age", all_of(outcomes), 'syear')
 
-list1 <- lapply(our_dataset[,-which(colnames(our_dataset) %in% no_factor)], factor)
-list2 <- lapply(our_dataset[,which(colnames(our_dataset) %in% no_factor)], as.numeric)
-data2 <- data.frame(list1, list2)
+list1 <- lapply(main_dataset[,-which(colnames(main_dataset) %in% no_factor)], factor)
+list2 <- lapply(main_dataset[,which(colnames(main_dataset) %in% no_factor)], as.numeric)
+imp_df <- data.frame(list1, list2)
 
 ## 2.2 Subset set data ---------------------------------------------------------
-data3 <- data2 %>% select(all_of(covariates), all_of(outcomes))
-data3[!data3$syear%in% years, outcomes] <- NA # make sure outcomes are NA in non-selected years
-data4 <- data3 %>% filter(syear %in% years) # if we want only the selected years
+imp_df <- imp_df %>% select(all_of(covariates), all_of(outcomes))
+imp_df[!imp_df$syear%in% years, outcomes] <- NA # make sure outcomes are NA in non-selected years
+imp_df <- imp_df %>% filter(syear %in% years) # if we want only the selected years
+
+
 
 ## 2.3 Predictor matrix and imputation ------------------------------------------
-pm <- make.predictorMatrix(data3)
+pm <- make.predictorMatrix(imp_df)
 pm[,'pid'] <- -2 # specify cluster variable pid 
 pm['pid', 'pid'] <- 0 # (but not for itself)
-pm[,'syear'] <- 0 # not sure here, but for now don't include syear as a predictor (gave a warning message if included as a predictor when it was a factor)
-# pm[outcomes,] <- 0 # if outcomes are not to be imputed
-pm
-#data5 <- drop_na(data4, all_of(outcomes)) # if we want to drop the outcome missings
+pm[,'syear'] <- 0 
 
-if(!exists("imp_long") & file.exists(file.path(getwd(), "data","imp_long.RData"))){ 
-  load(file.path(getwd(), "data","imp_long.RData")) 
-  print("imp_long.RData is loaded into R environment.")
-  } else if (!exists("imp_long") & !file.exists(file.path(getwd(), "data","imp_long.RData"))){ 
+if(!exists("imp_long") & file.exists(file.path(path_data,"imp_long.RData"))){ 
+  load(file.path(path_data,"imp_long.RData")) 
+  print("imp_long.RData is loaded from data folder into R environment.")
+  } else if (!exists("imp_long") & !file.exists(file.path(path_data, "imp_long.RData"))){ 
   library("blme")
   start.time <- Sys.time()
-  imp_long <- mice(data4, predictorMatrix = pm, 
+  imp_long <- mice(imp_df, predictorMatrix = pm, 
                    method = '2l.pmm', seed = 328,
                    maxit = 5, m = 5, blme_use=T, 
                    blme_args=list('fixef.prior'='normal'))
   
   end.time <- Sys.time()
   time.taken <- end.time - start.time
-  save(imp_long, file = "imp_long.RData")
+  save(imp_long, file = file.path(path_data,"imp_long.RData"))
   } 
-imp_list <- imp_long %>% complete('all') # extract five imputed datasets
+
+
+# extract five imputed datasets
+imp_list <- imp_long %>% complete('all') 
 
 
 
 
-## 1.4 Estimation results ------------------------------------------------------
-### 1.4.1  for Model 1 --------------------------------------------------------- 
+## 3 Estimation results --------------------------------------------------------
+### 3.1 for Model 1 ------------------------------------------------------------ 
 depvars <- c("culture", "cinema", "sports", "social", "volunteer", "help")
 estimateModel1_imp <- lapply(depvars, function(var) {
 est <- imp_list %>% 
@@ -88,11 +90,14 @@ est <- imp_list %>%
                             yearsedu + disabled + married + child1 +  child2 + 
                             child3plus + west | 
                             pid + syear, ..cluster = ~ pid, ..lhs = var)) %>% 
-  pool() %>% summary() %>% as.data.frame()
+  pool() 
+  df <- est %>% summary() %>% as.data.frame()
+  df$adjr <- pool.r.squared(est, adjusted = T)[1]
+  return(df)
 })
 names(estimateModel1_imp) <- depvars
 
-### 1.4.2  for Model 2 --------------------------------------------------------- 
+### 3.2  for Model 2 ----------------------------------------------------------- 
 depvars <- c("culture", "cinema", "sports", "social", "volunteer", "help")
 estimateModel2_imp <- lapply(depvars, function(var) {
   est <- imp_list %>% 
@@ -102,12 +107,16 @@ estimateModel2_imp <- lapply(depvars, function(var) {
                               yearsedu + disabled + married + child1 +  child2 + 
                               child3plus + west | 
                               pid + syear, ..cluster = ~ pid, ..lhs = var)) %>% 
-    pool() %>% summary() %>% as.data.frame()
+    pool() 
+  df <- est %>% summary() %>% as.data.frame()
+  df$adjr <- pool.r.squared(est, adjusted = T)[1]
+  return(df)
 })
 names(estimateModel2_imp) <- depvars
 # Test: 
 # estimateModel2_imp[[1]]
+imp_list[[1]]
 
-
-rm(data2,data3, data4, list1, list2, pm, covariates, depvars, i, no_factor, 
-   outcomes, start.time, toload, years, imp_list, imp_long)
+# remove irrelevant variables and dataframes from global console
+ rm(data2,data3, data4, list1, list2, pm, covariates, depvars, i, no_factor, 
+   outcomes, toload, years, imp_list, imp_df)
